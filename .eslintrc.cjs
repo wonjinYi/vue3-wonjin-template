717fe22272c176790217bf94e47e00e5875bedad
Wonjin Lee <studioplug17@gmail.com>
2025-04-24 17:51:50 +0900

    chore: Separate&update eslint, prettier settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,33 +1,20 @@
-// https://www.gymcoding.co/eslint-prettier
-
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution');
+require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
-	root: true,
-	extends: [
-		'plugin:vue/vue3-essential',
-		'eslint:recommended',
-		'@vue/eslint-config-prettier',
-	],
-	rules: {
-		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-		'prettier/prettier': [
-			'error',
-			{
-				singleQuote: true,
-				semi: true,
-				useTabs: true,
-				tabWidth: 2,
-				trailingComma: 'all',
-				printWidth: 80,
-				bracketSpacing: true,
-				arrowParens: 'avoid',
-				endOfLine: 'auto',
-			},
-		],
-	},
-	parserOptions: {
-		ecmaVersion: 'latest',
-	},
+  root: true,
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-prettier",
+  ],
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+  },
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  globals: {
+    defineModel: "readonly",
+  },
 };
